9f0608744ea0d69fb7c84e891a5a239eb237ab89
Danni Larsen (DLA) <dla@novicell.dk>
2017-05-11 10:28:53 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# novicell-overlay
+# Novicell Overlay
+**Simple script that can open an overlay / modal**
+
+## Usage
+
+Written in pure Vanilla JS, it has *no dependencies*. It ships with a sample LESS file, for easy implementation with the [novicell-frontend setup](https://github.com/Novicell/novicell-frontend). Use the styles for inspiration and then make your own styles.
+
+### Install with npm
+
+```bash
+npm install novicell-overlay --save
+```
+
+## Setup
+
+First include the js file in your js bundle or in your HTML:
+
+**JS bundle**
+```javascript
+scripts: [
+    vendorPath + "novicell-overlay/js/novicell.overlay.js"
+    ...
+]
+```
+
+**HTML**
+```html
+    <script src="/node_modules/novicell-overlay/js/novicell.overlay.js"></script>
+```
+
+## Example
+
+Set
+
+**Markup**
+```html
+
+```
+
+
+Then call the `novicell.overlay.set()`-method when you need to open the overlay.
+**Example:**
+```javascript
+document.addEventListener("DOMContentLoaded", function() {
+    var overlayTriggers = document.querySelectorAll('.open-overlay');
+
+    for (var i = 0; i < overlayTriggers.length; i++) {
+        var element = overlayTriggers[i];
+
+        element.addEventListener(function() {
+            novicell.overlay.set({ 'content':element.getAttribute('href'), 'class':'overlay-test' });
+        });
+    }
+    ...
+});
+```
+
+## Extension
+
+When extending the script, make sure to make a singleton for the `novicell` and the `novicell.overlay` objects before adding your own methods.
+
+```javascript
+'use strict';
+
+var novicell = novicell || {};
+novicell.overlay = novicell.overlay || {};
+novicell.overlay.extentions = novicell.overlay.extentions || new function () {
+    this.test = function() {
+        console.log('test');
+    };
+}();
+```
+Next you need to include your js-files in your js bundle or in your HTML, and then call the `init`-method from your `master.js`.
+Make sure to load you:
+
+**JS bundle**
+```javascript
+scripts: [
+    vendorPath + "novicell-overlay/js/novicell.overlay.js"
+    projectPath + "/components/novicell.overlay.extentions.js"
+    ...
+]
+```
+
+**HTML**
+```html
+    <script src="/node_modules/novicell-overlay/js/novicell.overlay.js"></script>
+    <script src="/scripts/components/novicell.overlay.extentions.js"></script>
+```
+
+Then call the `test`-method from your `master.js`:
+```javascript
+
+document.addEventListener("DOMContentLoaded", function() {
+    novicell.overlay.init();
+    novicell.overlay.extentions.test();
+    ...
+});
+```
---
+++ js/novicell.overlay.js
@@ -0,0 +1,190 @@
+'use strict';
+/**
+ * @name Novicell overlay
+ * @desc Simple script that opens an overlay / modal with some content form either a selector or an URL
+ * @author Danni Larsen - DLA & Michael SÃ¸lvsteen - MSL
+ * @example novicell.overlay.create({ 'selector': SELECTOR, 'url': URL,  'class':'CLASSNAME', 'onCreate': FUNCTIONNAME, 'onLoaded': FUNCTIONNAME, 'onDestroy': FUNCTIONNAME });
+ * @requires none
+ */
+
+var novicell = novicell || {};
+
+novicell.overlay = novicell.overlay || new function () {
+    var self = this;
+    var overlayElem;
+    var backdrop;
+    var options = {};
+    var content;
+    var onCreate;
+    var onLoaded;
+    var onDestroy;
+
+    this.create = function (opts) {
+        var self = this;
+        // Set global options
+        options = opts;
+
+        // Call onCreate callback
+        if (typeof options.onCreate === 'function') {
+            options.onCreate();
+        }
+
+        // Remove existing overlays
+        self.destroy();
+
+        // Check if content comes from a HTML element
+        if (options.hasOwnProperty('selector') && options.selector !== null) {
+            var element = document.querySelector(options.selector);
+            if (element) {
+                content = element.innerHTML;
+                constructOverlay();
+            } else {
+                console.warn('novicell.overlay: element does not exist. Please provide a valid selector for use in document.querySelector.');
+                return;
+            }
+        }
+        // Or if content comes from an URL
+        else if (options.hasOwnProperty('url')) {
+            if (options.url !== null) {
+                get(options.url).then(function (response) {
+                    content = response;
+                    constructOverlay();
+                }, function (error) {
+                    console.error("novicell.overlay:", error);
+                });
+            } else {
+                console.warn('novicell.overlay: url is empty. Please provide a url for use in xhr request.');
+                return;
+            }
+        }
+        // If nothing is working, send error to the console
+        else {
+            console.error('novicell.overlay: no content to display! /n Please set a selector or a url to load.')
+            return;
+        }
+    };
+
+    this.destroy = function () {
+        if(document.querySelector('#js-novi-overlay')) {
+            // Remove elements
+            overlayElem.parentElement.removeChild(overlayElem);
+            backdrop.parentElement.removeChild(backdrop);
+            // Stop listening for close overlay events
+            document.removeEventListener('keyup', self.destroy);
+            // Remove class on body
+            document.documentElement.classList.remove('no-scroll', 'novi-overlay--open');
+
+            // Call onDestroy callback
+            if (typeof options.onDestroy === 'function') {
+                options.onDestroy();
+            }
+        }
+    };
+
+    function constructOverlay() {
+        // Create the overlay
+        setupOverlay();
+        // Create close button
+        setupCloseButton();
+        // Create content for overlay
+        setupOverlayContent();
+        // Create backdrop
+        setupBackdrop();
+        // Add class to body-element
+        document.documentElement.classList.add('no-scroll', 'novi-overlay--open');
+        // Call onLoaded callback
+        if (typeof options.onLoaded === 'function') {
+            options.onLoaded();
+        }
+    };
+
+    /*
+     * Helper functions for HTML elements
+     */
+    function setupOverlay() {
+        // Create the overlay
+        overlayElem = document.createElement('div');
+        overlayElem.classList.add('novi-overlay');
+        overlayElem.id = 'js-novi-overlay';
+
+        // Set class for the overlay, if set in options
+        if (options.hasOwnProperty('class')) {
+            overlayElem.classList.add(options.class);
+        }
+        // Add overlay to overlay element
+        document.querySelector('body').appendChild(overlayElem);
+    };
+
+    function setupOverlayContent() {
+        // Create content for overlay
+        var overlayContent = document.createElement('div');
+        overlayContent.classList.add('novi-overlay__content');
+        // Create scroll element
+        var overlayScroll = document.createElement('div');
+        overlayScroll.classList.add('novi-overlay__scroll');
+        // Set content
+        overlayScroll.innerHTML = content;
+        overlayContent.appendChild(overlayScroll);
+        // Add overlayContent to overlay element
+        overlayElem.appendChild(overlayContent);
+    };
+
+    function setupCloseButton() {
+        // Create the button
+        var btnClose = document.createElement('button');
+        btnClose.classList.add('novi-overlay-close', 'button--close');
+        btnClose.type = 'button';
+        btnClose.id = 'js-novi-overlay-close';
+        // Add eventlistener for button click
+        btnClose.addEventListener('click', self.destroy);
+        // Add eventlistener for esc key
+        document.addEventListener('keydown', function (e) {
+            if (e.keyCode === 27) {
+                self.destroy();
+            }
+        });
+
+        // Add close button to overlay element
+        overlayElem.appendChild(btnClose);
+    };
+
+    function setupBackdrop() {
+        // Create the backdrop
+        backdrop = document.createElement('div');
+        backdrop.classList.add('novi-backdrop');
+        backdrop.id = 'js-novi-backdrop';
+        // Add backdrop to overlay element
+        document.querySelector('body').appendChild(backdrop);
+    };
+
+    /*
+     * Helper functions for getting content
+     */
+    function get(url) {
+        // Return a new promise.
+        return new Promise(function (resolve, reject) {
+            // Do the usual XHR stuff
+            var req = new XMLHttpRequest();
+            req.open('GET', url);
+
+            req.onload = function () {
+                if (req.status >= 200 && req.status < 400) {
+                    // Success!!
+                    resolve(req.response);
+                } else {
+                    // Error!!
+                    reject(Error(req.statusText));
+                }
+            };
+
+            // Handle network errors
+            req.onerror = function () {
+                reject(Error("Network Error"));
+            };
+
+            // Make the request
+            req.send();
+        });
+    };
+
+}();

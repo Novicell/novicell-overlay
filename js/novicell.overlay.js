827c64303c36b974d5449854fa87fb4b9785044a
shp@novicell.dk <shp@novicell.dk>
2017-05-22 15:17:03 +0200

    Big improvements for novi-overlay

--- js/novicell.overlay.js
+++ js/novicell.overlay.js
@@ -1,8 +1,8 @@
-'use strict';
+﻿'use strict';
 /**
  * @name Novicell overlay
  * @desc Simple script that opens an overlay / modal with some content form either a selector or an URL
- * @author Danni Larsen - DLA & Michael SÃ¸lvsteen - MSL
+ * @author Danni Larsen (DLA), Michael Sølvsteen (MSL), Signe Helbo Poulsen (SHP)
  * @example novicell.overlay.create({ 'selector': SELECTOR, 'url': URL,  'class':'CLASSNAME', 'onCreate': FUNCTIONNAME, 'onLoaded': FUNCTIONNAME, 'onDestroy': FUNCTIONNAME });
  * @requires none
  */
@@ -11,9 +11,11 @@ var novicell = novicell || {};
 
 novicell.overlay = novicell.overlay || new function () {
     var self = this;
-    var overlayElem;
-    var backdrop;
     var options = {};
+    var overlayElem;
+    var overlayContent;
+    var overlayScroll;
+    var backdrop;
     var content;
     var onCreate;
     var onLoaded;
@@ -35,6 +37,7 @@ novicell.overlay = novicell.overlay || new function () {
         // Check if content comes from a HTML element
         if (options.hasOwnProperty('selector') && options.selector !== null) {
             var element = document.querySelector(options.selector);
+            
             if (element) {
                 content = element.innerHTML;
                 constructOverlay();
@@ -69,8 +72,10 @@ novicell.overlay = novicell.overlay || new function () {
             // Remove elements
             overlayElem.parentElement.removeChild(overlayElem);
             backdrop.parentElement.removeChild(backdrop);
+
             // Stop listening for close overlay events
             document.removeEventListener('keyup', self.destroy);
+
             // Remove class on body
             document.documentElement.classList.remove('no-scroll', 'novi-overlay--open');
 
@@ -82,20 +87,41 @@ novicell.overlay = novicell.overlay || new function () {
     };
 
     function constructOverlay() {
-        // Create the overlay
-        setupOverlay();
-        // Create close button
-        setupCloseButton();
-        // Create content for overlay
-        setupOverlayContent();
         // Create backdrop
         setupBackdrop();
+
+        // Create the overlay
+        setupOverlay();
+
+        // Create content for overlay
+        setupOverlayContent();
+
+        // Create close button
+        setupCloseButton();
+
         // Add class to body-element
-        document.documentElement.classList.add('no-scroll', 'novi-overlay--open');
+        document.documentElement.classList.add('no-scroll');
+
         // Call onLoaded callback
         if (typeof options.onLoaded === 'function') {
             options.onLoaded();
         }
+    };
+
+    function setupBackdrop() {
+        // Create the backdrop
+        backdrop = document.createElement('div');
+        backdrop.classList.add('novi-backdrop');
+        backdrop.id = 'js-novi-backdrop';
+
+        backdrop.addEventListener('click', function(e){
+            if(e.target.classList.contains('novi-overlay') || e.target.classList.contains('novi-overlay__content')) {
+                self.destroy();
+            }
+        });
+
+        // Add backdrop to overlay element
+        document.querySelector('body').appendChild(backdrop);
     };
 
     /*
@@ -111,20 +137,25 @@ novicell.overlay = novicell.overlay || new function () {
         if (options.hasOwnProperty('class')) {
             overlayElem.classList.add(options.class);
         }
+
         // Add overlay to overlay element
-        document.querySelector('body').appendChild(overlayElem);
+        // document.querySelector('body').appendChild(overlayElem);
+        backdrop.appendChild(overlayElem);
     };
 
     function setupOverlayContent() {
         // Create content for overlay
-        var overlayContent = document.createElement('div');
+        overlayContent = document.createElement('div');
         overlayContent.classList.add('novi-overlay__content');
+
         // Create scroll element
-        var overlayScroll = document.createElement('div');
+        overlayScroll = document.createElement('div');
         overlayScroll.classList.add('novi-overlay__scroll');
+
         // Set content
         overlayScroll.innerHTML = content;
         overlayContent.appendChild(overlayScroll);
+
         // Add overlayContent to overlay element
         overlayElem.appendChild(overlayContent);
     };
@@ -135,8 +166,10 @@ novicell.overlay = novicell.overlay || new function () {
         btnClose.classList.add('novi-overlay-close', 'button--close');
         btnClose.type = 'button';
         btnClose.id = 'js-novi-overlay-close';
+
         // Add eventlistener for button click
         btnClose.addEventListener('click', self.destroy);
+
         // Add eventlistener for esc key
         document.addEventListener('keydown', function (e) {
             if (e.keyCode === 27) {
@@ -145,16 +178,7 @@ novicell.overlay = novicell.overlay || new function () {
         });
 
         // Add close button to overlay element
-        overlayElem.appendChild(btnClose);
-    };
-
-    function setupBackdrop() {
-        // Create the backdrop
-        backdrop = document.createElement('div');
-        backdrop.classList.add('novi-backdrop');
-        backdrop.id = 'js-novi-backdrop';
-        // Add backdrop to overlay element
-        document.querySelector('body').appendChild(backdrop);
+        overlayScroll.appendChild(btnClose);
     };
 
     /*

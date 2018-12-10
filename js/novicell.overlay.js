c9913d13e1c001d2264a9835beb7b5e808393b98
tbb2 <tbb@novicell.dk>
2018-12-10 10:35:37 +0100

    Added support for multiple close buttons
    Since both Mette, Kasper and I recently had the need for extra close buttons in the overlays, I added support for that. Just have to add the class "js-novi-overlay-close" to a tag inside the backdrop

--- js/novicell.overlay.js
+++ js/novicell.overlay.js
@@ -1,7 +1,7 @@
-﻿'use strict';
+'use strict';
 /**
  * @name Novicell overlay
- * @desc Simple script that opens an overlay / modal with some content form either a selector or an URL
+ * @description Simple script that opens an overlay / modal with some content form either a selector or an URL. To have more close-buttons inside the overlay, use the css class js-novi-overlay-close
  * @author Danni Larsen (DLA), Michael Sølvsteen (MSL), Signe Helbo Poulsen (SHP), Emil Skytte Ankersen (EAN)
  * @example novicell.overlay.create({ 'selector': SELECTOR, 'url': URL,  'class':'CLASSNAME', 'onCreate': FUNCTIONNAME, 'onLoaded': FUNCTIONNAME, 'onDestroy': FUNCTIONNAME });
  * @requires none
@@ -9,245 +9,283 @@
 
 var novicell = novicell || {};
 
-novicell.overlay = novicell.overlay || new function () {
-    var self = this;
-    var options = {};
-    var overlayElem;
-    var overlayContainer;
-    var overlayContent;
-    var backdrop;
-    var content;
-    var onCreate;
-    var onLoaded;
-    var onDestroy;
-    var isVideo = false;
-
-    this.create = function (opts) {
+novicell.overlay =
+    novicell.overlay ||
+    new function() {
         var self = this;
-        // Set global options
-        options = opts;
+        var options = {};
+        var overlayElem;
+        var overlayContainer;
+        var overlayContent;
+        var backdrop;
+        var content;
+        var onCreate;
+        var onLoaded;
+        var onDestroy;
+        var isVideo = false;
 
-        // Call onCreate callback
-        if (typeof options.onCreate === 'function') {
-            options.onCreate();
-        }
+        this.create = function(opts) {
+            var self = this;
+            // Set global options
+            options = opts;
 
-        // Remove existing overlays
-        self.destroy();
-
-        // Check if content comes from a DOM selector
-        if (options.hasOwnProperty('selector') && options.selector !== null) {
-            var element = document.querySelector(options.selector);
-            
-            if (element) {
-                content = element.innerHTML;
-                constructOverlay();
-            } else {
-                console.warn('novicell.overlay: element does not exist. Please provide a valid selector for use in document.querySelector.');
-                return;
+            // Call onCreate callback
+            if (typeof options.onCreate === 'function') {
+                options.onCreate();
             }
-        }
 
-        // Check if content comes from a HTML element
-        else if (options.hasOwnProperty('element') && options.element !== null) {
-            var element = options.element;
+            // Remove existing overlays
+            self.destroy();
 
-            if (element) {
-                content = element.innerHTML;
-                constructOverlay();
-            } else {
-                console.warn('novicell.overlay: element does not exist. Please provide a valid DOM element.');
-                return;
-            }
-        }
+            // Check if content comes from a DOM selector
+            if (
+                options.hasOwnProperty('selector') &&
+                options.selector !== null
+            ) {
+                var element = document.querySelector(options.selector);
 
-        // Or if content comes from an ID
-        else if (options.hasOwnProperty('videoId')) {
-            if (options.videoId !== null) {
-                var src = '';
-                isVideo = true;
-
-                if(options.type == 'vimeo') {
-                    src = 'https://player.vimeo.com/video/' + options.videoId + '?autoplay=' + options.autoplay;
-                }
-                else if(options.type == 'youtube') {
-                    src = 'https://www.youtube.com/embed/' + options.videoId + '?autoplay=' + options.autoplay + '&rel=0';
-                }
-                else {
+                if (element) {
+                    content = element.innerHTML;
+                    constructOverlay();
+                } else {
+                    console.warn(
+                        'novicell.overlay: element does not exist. Please provide a valid selector for use in document.querySelector.'
+                    );
                     return;
                 }
+            }
 
-                var iframe = document.createElement('iframe');
-                iframe.setAttribute('src', src);
-                iframe.setAttribute('frameborder', 0);
-                iframe.setAttribute('allowfullscreen', '');
-                iframe.setAttribute('width', '100%');
-                iframe.setAttribute('height', '100%');
+            // Check if content comes from a HTML element
+            else if (
+                options.hasOwnProperty('element') &&
+                options.element !== null
+            ) {
+                var element = options.element;
 
-                content = iframe.outerHTML;
-                
-                constructOverlay();
-            } else {
-                console.warn('novicell.overlay: video-id is empty. Please provide a video-id for use in video embed code (we support only Vimeo and YouTube).');
+                if (element) {
+                    content = element.innerHTML;
+                    constructOverlay();
+                } else {
+                    console.warn(
+                        'novicell.overlay: element does not exist. Please provide a valid DOM element.'
+                    );
+                    return;
+                }
+            }
+
+            // Or if content comes from an ID
+            else if (options.hasOwnProperty('videoId')) {
+                if (options.videoId !== null) {
+                    var src = '';
+                    isVideo = true;
+
+                    if (options.type == 'vimeo') {
+                        src =
+                            'https://player.vimeo.com/video/' +
+                            options.videoId +
+                            '?autoplay=' +
+                            options.autoplay;
+                    } else if (options.type == 'youtube') {
+                        src =
+                            'https://www.youtube.com/embed/' +
+                            options.videoId +
+                            '?autoplay=' +
+                            options.autoplay +
+                            '&rel=0';
+                    } else {
+                        return;
+                    }
+
+                    var iframe = document.createElement('iframe');
+                    iframe.setAttribute('src', src);
+                    iframe.setAttribute('frameborder', 0);
+                    iframe.setAttribute('allowfullscreen', '');
+                    iframe.setAttribute('width', '100%');
+                    iframe.setAttribute('height', '100%');
+
+                    content = iframe.outerHTML;
+
+                    constructOverlay();
+                } else {
+                    console.warn(
+                        'novicell.overlay: video-id is empty. Please provide a video-id for use in video embed code (we support only Vimeo and YouTube).'
+                    );
+                    return;
+                }
+            }
+            // If nothing is working, send error to los consolé
+            else {
+                console.error(
+                    'novicell.overlay: no content to display! Please set a selector or a url to load.'
+                );
                 return;
             }
-        }
-        // If nothing is working, send error to los consolé
-        else {
-            console.error('novicell.overlay: no content to display! Please set a selector or a url to load.')
-            return;
-        }
-    };
+        };
 
-    this.destroy = function () {
-        if(document.querySelector('#js-novi-overlay')) {
-            // Remove elements
-            overlayElem.parentElement.removeChild(overlayElem);
-            backdrop.parentElement.removeChild(backdrop);
+        this.destroy = function() {
+            if (document.querySelector('#js-novi-overlay')) {
+                // Remove elements
+                overlayElem.parentElement.removeChild(overlayElem);
+                backdrop.parentElement.removeChild(backdrop);
 
-            // Stop listening for close overlay events
-            document.removeEventListener('keyup', self.destroy);
+                // Stop listening for close overlay events
+                document.removeEventListener('keyup', self.destroy);
 
-            // Remove class on body
-            document.documentElement.classList.remove('no-scroll', 'novi-overlay--open');
+                // Remove class on body
+                document.documentElement.classList.remove(
+                    'no-scroll',
+                    'novi-overlay--open'
+                );
 
-            // Reset video variable
-            isVideo = false;
+                // Reset video variable
+                isVideo = false;
 
-            // Call onDestroy callback
-            if (typeof options.onDestroy === 'function') {
-                options.onDestroy();
-            }
-        }
-    };
-
-    function constructOverlay() {
-        // Create backdrop
-        setupBackdrop();
-
-        // Create the overlay
-        setupOverlay();
-
-        // Create content for overlay
-        setupOverlayContainer();
-
-        // Create close button
-        setupCloseButton();
-
-        // Add class to body-element
-        document.documentElement.classList.add('no-scroll');
-
-        // Call onLoaded callback
-        if (typeof options.onLoaded === 'function') {
-            options.onLoaded();
-        }
-    };
-
-    function setupBackdrop() {
-        // Create the backdrop
-        backdrop = document.createElement('div');
-        backdrop.classList.add('novi-backdrop');
-        backdrop.id = 'js-novi-backdrop';
-
-        backdrop.addEventListener('click', function(e){
-            if(e.target.classList.contains('novi-overlay') || e.target.classList.contains('novi-overlay__container')) {
-                self.destroy();
-            }
-        });
-
-        // Add backdrop to overlay element
-        document.querySelector('body').appendChild(backdrop);
-    };
-
-    /*
-     * Helper functions for HTML elements
-     */
-    function setupOverlay() {
-        // Create the overlay
-        overlayElem = document.createElement('div');
-        overlayElem.classList.add('novi-overlay');
-        overlayElem.id = 'js-novi-overlay';
-
-        // Set class for the overlay, if set in options
-        if (options.hasOwnProperty('class')) {
-            overlayElem.classList.add(options.class);
-        }
-
-        // Add overlay to overlay element
-        // document.querySelector('body').appendChild(overlayElem);
-        backdrop.appendChild(overlayElem);
-    };
-
-    function setupOverlayContainer() {
-        // Create content for overlay
-        overlayContainer = document.createElement('div');
-        overlayContainer.classList.add('novi-overlay__container');
-
-        // Create scroll element
-        overlayContent = document.createElement('div');
-        overlayContent.classList.add('novi-overlay__content');
-
-        if(isVideo) {
-            overlayContent.classList.add('novi-overlay__content--video')
-        }
-
-        // Set content
-        overlayContent.innerHTML = content;
-        overlayContainer.appendChild(overlayContent);
-
-        // Add overlayContainer to overlay element
-        overlayElem.appendChild(overlayContainer);
-    };
-
-    function setupCloseButton() {
-        // Create the button
-        var btnClose = document.createElement('button');
-        btnClose.classList.add('novi-overlay-close', 'button--close');
-        btnClose.type = 'button';
-        btnClose.id = 'js-novi-overlay-close';
-
-        // Add eventlistener for button click
-        btnClose.addEventListener('click', self.destroy);
-
-        // Add eventlistener for esc key
-        document.addEventListener('keydown', function (e) {
-            if (e.keyCode === 27) {
-                self.destroy();
-            }
-        });
-
-        // Add close button to overlay element
-        overlayContent.appendChild(btnClose);
-    };
-
-    /*
-     * Helper functions for getting content
-     */
-    function get(url) {
-        // Return a new promise.
-        return new Promise(function (resolve, reject) {
-            // Do the usual XHR stuff
-            var req = new XMLHttpRequest();
-            req.open('GET', url);
-
-            req.onload = function () {
-                if (req.status >= 200 && req.status < 400) {
-                    // Success!!
-                    resolve(req.response);
-                } else {
-                    // Error!!
-                    reject(Error(req.statusText));
+                // Call onDestroy callback
+                if (typeof options.onDestroy === 'function') {
+                    options.onDestroy();
                 }
-            };
+            }
+        };
 
-            // Handle network errors
-            req.onerror = function () {
-                reject(Error("Network Error"));
-            };
+        function constructOverlay() {
+            // Create backdrop
+            setupBackdrop();
 
-            // Make the request
-            req.send();
-        });
-    };
+            // Create the overlay
+            setupOverlay();
 
-}();
+            // Create content for overlay
+            setupOverlayContainer();
+
+            // Create close button
+            setupCloseButton();
+
+            // Add class to body-element
+            document.documentElement.classList.add('no-scroll');
+
+            // Call onLoaded callback
+            if (typeof options.onLoaded === 'function') {
+                options.onLoaded();
+            }
+        }
+
+        function setupBackdrop() {
+            // Create the backdrop
+            backdrop = document.createElement('div');
+            backdrop.classList.add('novi-backdrop');
+            backdrop.id = 'js-novi-backdrop';
+
+            backdrop.addEventListener('click', function(e) {
+                if (
+                    e.target.classList.contains('novi-overlay') ||
+                    e.target.classList.contains('novi-overlay__container')
+                ) {
+                    self.destroy();
+                }
+            });
+
+            // Add backdrop to overlay element
+            document.querySelector('body').appendChild(backdrop);
+        }
+
+        /*
+         * Helper functions for HTML elements
+         */
+        function setupOverlay() {
+            // Create the overlay
+            overlayElem = document.createElement('div');
+            overlayElem.classList.add('novi-overlay');
+            overlayElem.id = 'js-novi-overlay';
+
+            // Set class for the overlay, if set in options
+            if (options.hasOwnProperty('class')) {
+                overlayElem.classList.add(options.class);
+            }
+
+            // Add overlay to overlay element
+            // document.querySelector('body').appendChild(overlayElem);
+            backdrop.appendChild(overlayElem);
+        }
+
+        function setupOverlayContainer() {
+            // Create content for overlay
+            overlayContainer = document.createElement('div');
+            overlayContainer.classList.add('novi-overlay__container');
+
+            // Create scroll element
+            overlayContent = document.createElement('div');
+            overlayContent.classList.add('novi-overlay__content');
+
+            if (isVideo) {
+                overlayContent.classList.add('novi-overlay__content--video');
+            }
+
+            // Set content
+            overlayContent.innerHTML = content;
+            overlayContainer.appendChild(overlayContent);
+
+            // Add overlayContainer to overlay element
+            overlayElem.appendChild(overlayContainer);
+        }
+
+        function setupCloseButton() {
+            // Create the button
+            var btnClose = document.createElement('button');
+            btnClose.classList.add(
+                'novi-overlay-close',
+                'button--close',
+                'js-novi-overlay-close'
+            );
+            btnClose.type = 'button';
+
+            // Add eventlistener for esc key
+            document.addEventListener('keydown', function(e) {
+                if (e.keyCode === 27) {
+                    self.destroy();
+                }
+            });
+
+            // Add close button to overlay element
+            overlayContent.appendChild(btnClose);
+
+            //find all available close buttons inside the backdrop - also the ones not generated from the script
+            var closeButtons = document.querySelectorAll(
+                '#js-novi-backdrop .js-novi-overlay-close'
+            );
+            for (var i = 0; i < closeButtons.length; i++) {
+                closeButtons[i].addEventListener('click', function() {
+                    self.destroy();
+                });
+            }
+        }
+
+        /*
+         * Helper functions for getting content
+         */
+        function get(url) {
+            // Return a new promise.
+            return new Promise(function(resolve, reject) {
+                // Do the usual XHR stuff
+                var req = new XMLHttpRequest();
+                req.open('GET', url);
+
+                req.onload = function() {
+                    if (req.status >= 200 && req.status < 400) {
+                        // Success!!
+                        resolve(req.response);
+                    } else {
+                        // Error!!
+                        reject(Error(req.statusText));
+                    }
+                };
+
+                // Handle network errors
+                req.onerror = function() {
+                    reject(Error('Network Error'));
+                };
+
+                // Make the request
+                req.send();
+            });
+        }
+    }();
